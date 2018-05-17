2a838bc7a3896e4f284d729833cbeebb5c92d584
ridaamirini <ridaamirini@rs-apps.de>
2018-05-17 05:00:55 +0200

    WIP vNodes JSX

--- example/App.vue
+++ example/App.vue
@@ -1,8 +1,15 @@
 <template>
     <div>
-
         <vue-cloneya>
-            <input type="text" name="test[]" ref="placeholder" slot="content">
+            <div class="col-md-8">
+                <div class="input-group m-t-10 toclone cloneya">
+                    <input type="text" id="phone" name="phone[]" class="form-control p-l-0" placeholder="Telefon" value="" required="" v-cloneya-input>
+                    <span class="input-group-btn" slot="action">
+                        <button type="button" class="btn btn-success clone" tabindex="-1" v-cloneya-add-btn><i class="fa fa-plus"></i></button>
+                        <button type="button" class="btn btn-danger delete" tabindex="-1" v-cloneya-remove-btn><i class="fa fa-minus"></i></button>
+                    </span>
+                </div>
+            </div>
         </vue-cloneya>
     </div>
 </template>
--- src/index.js
+++ src/index.js
@@ -6,33 +6,73 @@ export const createCloneYa = (opts = {}) => {
         props: {},
         data() {
             return {
-                test: 'Hello World JSX!'
+                test: 'Hello World JSX!',
+                renderData: [{}],
             }
         },
         mounted() {},
         beforeDestroy() {},
-        render(h) {
-            returnh('div', {key : el, style : {width:'inherit'}},
-                [
-                    h('div', {style : {width:'80%', display:'inline'}}, vNodes),
-                    //button [-] :
-                    ctx.length > 1      ? h('input',  {attrs:{type:'button', value:'-', index : index}, on:{click:_this.del}} ) : null,
-                    //button [+] :
-                    index==ctx.length-1 ? h('input',  {attrs:{type:'button', value:'+'}, on:{click:_this.add}} ): null
-                ]
-            );
+        render: function (h) {
+            let _this = this;
+            let vNodes = _this.$slots.default;
+
+            console.log(vNodes);
+
+            return h('div', {
+                    class : {
+                        'clone-wrapper': true
+                    }
+                },
+                this.renderData.map(function (el, index, ctx){
+                    console.log(index);
+
+                        return h('div', {
+                            key: index,
+                            class: {
+                                'toClone': true
+                            },
+                            on: {
+                                click: _this.add
+                            }
+                        },
+                        [
+                            vNodes,
+                            //button [-] :
+                            //ctx.length > 1      ? h('input',  {attrs:{type:'button', value:'-', index: index}, on:{click:_this.del}} ) : null,
+                            //button [+] :
+                            //index==ctx.length-1 ? h('input',  {attrs:{type:'button', value:'+'}, on:{click:_this.add}} ): null
+                        ]
+                    )
+                })
+            )
         },
-        methods: {}
+        methods: {
+            add(){
+                this.renderData.push({});
+            },
+            del(el){
+                this.renderData.splice( el.srcElement.attributes.index.value , 1);
+            }
+        }
     }
 };
 
 export const install = (Vue, opts) => {
     const Component = createCloneYa(opts);
     Vue.component(Component.name, Component);
-    /*Vue.directive('cloneya', function (el) {
-        el.style.display = 'none';
-        el.disabled = true; // To be ignored by submitting
-    });*/
+
+
+    Vue.directive('cloneyaInput', function (el) {
+        el.classList.add('vcloneya');
+    });
+
+    Vue.directive('cloneyaAddBtn', function (el) {
+        el.classList.add('vcloneya-add-btn');
+    });
+
+    Vue.directive('cloneyaRemoveBtn', function (el) {
+        el.classList.add('vcloneya-remove-btn');
+    });
 };
 
 export default install;
