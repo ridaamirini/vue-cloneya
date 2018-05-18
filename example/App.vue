d03ed99d230222b41c18c9de63a926c5cb4dd838
ridaamirini <ridaamirini@rs-apps.de>
2018-05-18 20:47:10 +0200

    v1.0.4 :raised_hands: better "uniqueId"

--- example/App.vue
+++ example/App.vue
@@ -8,11 +8,10 @@
         <div class="row" style="padding: 30px;">
             <div class="col-md-6 col-md-offset-4">
                 <div class="col-md-8">
-                    <vue-cloneya :minimum="3" :maximum="5" :value="exampleData" v-on:minimum:cloneya="alert('min')" v-on:maximum:cloneya="alert('max')">
+                    <vue-cloneya :minimum="1"  :value="exampleData" v-on:minimum:cloneya="alert('min')" v-on:maximum:cloneya="alert('max')">
                         <div class="input-group" style="margin-top: 10px;">
                             <input type="text" name="example[]" class="form-control" placeholder="Example" v-cloneya-input>
                             <span class="input-group-btn">
-                                <!-- add to "v-cloneya-add-btn to your element that you whatet to trigger add() -->
                                 <button type="button" class="btn btn-success clonspan" tabindex="-1" v-cloneya-add><i class="fa fa-plus"></i></button>
                                 <button type="button" class="btn btn-danger delete" tabindex="-1"  v-cloneya-remove><i class="fa fa-minus"></i></button>
                             </span>
