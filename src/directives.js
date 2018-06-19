824a0491353e841af6e2d582eb930d7598689474
ridaamirini <ridaamirini@rs-apps.de>
2018-06-19 03:29:54 +0200

    Cleaned code structure

---
+++ src/directives.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+
+const directives = {
+    cloneyaInput: function (el) {
+        el.classList.add('vcloneya');
+    },
+    cloneyaAdd: function (el) {
+        el.classList.add('vcloneya-add');
+    },
+    cloneyaRemove: function (el) {
+        el.classList.add('vcloneya-remove');
+    }
+};
+
+
+export const installDirectives = () => {
+    for (let name in directives) Vue.directive(name, directives[name]);
+};
+
+export default installDirectives;
